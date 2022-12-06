c0c180501186fb1991a960c1d7e39e45a4395b12
ilya-petrashkevich <ilya.petrashkevich@gmail.com>
2022-12-06 14:13:14 +0300

    first commit

---
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -0,0 +1,85 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
+import Greeting from './Greeting'
+import {UserType} from './HW3'
+
+type GreetingContainerPropsType = {
+    users: UserType[] // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
+}
+
+export const pureAddUser = (name: string, setError: (error: string) => void, setName: (name: string) => void,
+                            addUserCallback: (name: string) => void) => {
+    // ТЕСТ ПРОЙДЕН!!! если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+    if (name.trim()) {
+        setName(name.trim());
+        addUserCallback(name.trim());
+        setName('');
+    } else {
+        setError('Ошибка! Введите имя!');
+    }
+
+
+}
+
+export const pureOnBlur = (name: string, setError: (error: string) => void) => { // ТЕСТ ПРОЙДЕН если имя пустое - показать ошибку
+    if (!name.trim()) {
+        setError('Ошибка! Введите имя!');
+    } else {
+        name.trim()
+    }
+}
+
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => { // ТЕСТ ПРОЙДЕН НО ХЗ если нажата кнопка Enter - добавить
+    if (e.key === 'Enter') {
+        addUser();
+    }
+
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
+    // деструктуризация пропсов
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value)// need to fix
+
+        error && setError('')
+    }
+    const addUser = () => {
+        pureAddUser(name, setError, setName, addUserCallback)
+    }
+
+    const onBlur = () => {
+        pureOnBlur(name, setError)
+    }
+
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        pureOnEnter(e, addUser)
+    }
+
+    const totalUsers = users.length // need to fix 0
+    const lastUserName = users.length ? users[totalUsers - 1].name : ''; // СДЕЛАНО need to fix  'some name'
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            onBlur={onBlur}
+            onEnter={onEnter}
+            error={error}
+            totalUsers={totalUsers}
+            lastUserName={lastUserName}
+        />
+    )
+}
+
+export default GreetingContainer
---
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -0,0 +1,90 @@
+import React, {MouseEvent, useState} from 'react'
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import {restoreState} from '../hw06/localStorage/localStorage'
+import s from './Clock.module.css'
+
+function Clock() {
+    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+    const [show, setShow] = useState<boolean>(false)
+
+    const start = () => {
+        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
+        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+        setTimerId(+setInterval(() => setDate(new Date()), 1000))
+        // setInterval(() => setDate(new Date()), 1000);
+
+    }
+
+    const stop = () => {
+        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+        clearInterval(timerId)
+        setTimerId(undefined)
+    }
+
+    const onMouseEnter = (/*event: MouseEvent<HTMLDivElement, MouseEvent>*/) => { // пишут студенты // показать дату если наведена мышка
+        // return  event.name === 'mouseover' ? setShow(true) : '';
+        setShow(true)
+    }
+    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+        setShow(false)
+    }
+
+    const stringTime = date.toTimeString().slice(0, 8) || <br/> //'date->time'   .toTimeString().slice(0,8) || <br/>  // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = `${date.getDay()}.${date.getMonth()+1}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+    const stringDay = new Intl.DateTimeFormat('en-US', { weekday: 'long'}).format(date)/*date.getDay()*/ || <br/> // пишут студенты
+    const stringMonth = date.toLocaleString("en-US", {month: 'long'}) /*date.getMonth()+1*/ || <br/> // пишут студенты
+
+    return (
+        <div className={s.clock}>
+            <div
+                id={'hw9-watch'}
+                className={s.watch}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                <span id={'hw9-day'}>{stringDay}</span>,{' '}
+                <span id={'hw9-time'}>
+                    <strong>{stringTime}</strong>
+                </span>
+            </div>
+
+            <div id={'hw9-more'}>
+                <div className={s.more}>
+                    {show ? (
+                        <>
+                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                            <span id={'hw9-date'}>{stringDate}</span>
+                        </>
+                    ) : (
+                        <>
+                            <br/>
+                        </>
+                    )}
+                </div>
+            </div>
+
+            <div className={s.buttonsContainer}>
+                <SuperButton
+                    id={'hw9-button-start'}
+                    disabled={!!timerId} // true пишут студенты // задизэйблить если таймер запущен
+                    onClick={start}
+                >
+                    start
+                </SuperButton>
+                <SuperButton
+                    id={'hw9-button-stop'}
+                    disabled={!timerId} // true пишут студенты // задизэйблить если таймер не запущен
+                    onClick={stop}
+                >
+                    stop
+                </SuperButton>
+            </div>
+        </div>
+    )
+}
+
+export default Clock
